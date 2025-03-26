421aea3a93fc6e4a928e2304e910390572454b8f
shohan12727 <aishohan001@gmail.com>
2025-03-26 20:57:42 +0600

    two done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-alert('fuck cse');
+function cashOut(money) {
+    
+    if (typeof money !== 'number' || money < 0) {
+        return "Invalid";
+    }
+    
+
+
+    return money * 1.75 / 100;
+}
+
+
+function validEmail(email) {
+   
+    if (typeof email !== 'string') {
+        return "Invalid";
+    }
+    
+    
+    if (/^[\.\-_+@]/.test(email)) {
+        return false;
+    }
+   
+    if (!/@/.test(email) || email.indexOf('@') === 0 || email.lastIndexOf('@') !== email.indexOf('@')) {
+        return false;
+    }
+    
+
+    if (/\s/.test(email)) {
+        return false;
+    }
+    
+    if (!email.endsWith(".com")) {
+        return false;
+    }
+    
+    return true;
+}
+
